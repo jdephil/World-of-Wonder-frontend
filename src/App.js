6e8615d6ca0cf455345e05bba989caaf9c14b74f
Eryoung-610 <eryoung610@gmail.com>
2020-07-24 02:08:16 -0700

    Created collections, implement navbar as example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import Office from './components/Office'
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <Office />
+    <Navbar />
   );
 }
 
