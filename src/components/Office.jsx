35747d4856aa3eaf35483d63ec9c8fb047d6c183
jdephil <jennifer.dephillips@gmail.com>
2020-07-25 18:55:44 -0400

    placing office divs/links

--- src/components/Office.jsx
+++ src/components/Office.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Office.css'
 
 const Office = () => {
+
     return (
-        <div class = "container">
-            <img src='./office1.png' alt='Office with artifacts'/>
-        </div>
+            <main>
+                <Link className='egyptPortalLink col l6' to='/ancientEgypt'>Ancient Egypt</Link>
+                <Link className='pacificPortalLink' to='/pacific'>Pacific</Link>
+                <Link className='nativePortalLink' to='/nativeAmerican'>Native American</Link>
+                <img className='officeImg' src='./office1.png' alt='Office with artifacts'/>
+            </main>
     );
 };
 
