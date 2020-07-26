36dd76e6abe8893741f589a232af9a60d8f55be7
Eryoung-610 <eryoung610@gmail.com>
2020-07-26 15:54:59 -0700

    Images added into egypt hall

--- src/components/AncientEgypt.jsx
+++ src/components/AncientEgypt.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './AncientEgypt.css'
 
 const AncientEgypt = () => {
     return (
-        <div>
-            
+        <div class = "row">
+            <div className = "">
+                <img className="cosmetic hoverable" src="/cosmetics-egypt.png" alt="" />
+            </div>
+            <div className = "">
+                <img className="cosmetic hoverable" src="/funeral-beads-egypt.png" alt="" />
+            </div>
+            <div className = "">
+                <img className="cosmetic hoverable" src="/mummified-birds-egypt.png" alt="" />
+            </div>
+            {/* <div className = "hoverable">
+                <img className="mummy" src="/mummy-egypt.png" alt="" />
+            </div> */}
+            <div>
+                <img className="mummies hoverable" src="/mummy2-egypt.png" alt="" />
+            </div>
+            <div className = "hoverable">
+                <img className="slipper" src="/slippers-egypt.png" alt="" />
+            </div>
+            <div className = "hoverable">
+                <img className="writing" src="/wall-hanging-egypt.png" alt="" />
+            </div>
+            <div className = "hoverable">
+                <img className="cosmetic" src="/writings-egypt.png" alt="" />
+            </div>
+
         </div>
     );
 };
