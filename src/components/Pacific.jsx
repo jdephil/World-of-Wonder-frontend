6ce2254c9ada3b6726b1e0139cbecb5c7905fd32
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-07-27 13:10:00 -0700

    working on pacific functionality and added modal folder

--- src/components/Pacific.jsx
+++ src/components/Pacific.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import './Pacific.css'
+import React, { useState } from 'react';
+import './Pacific.css';
+import axios from 'axios';
 
 const Pacific = () => {
+    const [artifacts, setArtifacts] = useState({
+        name: "",
+        description: "", 
+        imageURL: ""
+      })
+
+    const showArtifactInfo = () => {
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/artifact`, artifacts)
+            .then(response => {
+
+            })
+    }
+
+
+
+
+
+
     return (
         <div>
             <div  className='pacificDiv'>
-            <img className='fishingHook pacificArtifact' src='/fishing-hook-pacific.png' alt='fishing hook' />
+            <img className='fishingHook pacificArtifact modalButtonEvent' src='/fishing-hook-pacific.png' alt='fishing hook' onClick={() => showArtifactInfo()} />
             <img className='fishingBox pacificArtifact' src='/fishing-box-pacific.png' alt='fishing box' />
             <img className='danceDress pacificArtifact' src='/dance-dress-pacific.png' alt='dance dress' />
             <img className='fan pacificArtifact' src='/fan-pacific.png' alt='fan' />
