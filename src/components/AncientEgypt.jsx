db434472e061901425f7e9a4bd222277387526f2
Eryoung-610 <eryoung610@gmail.com>
2020-07-27 01:35:46 -0700

    Initial locations set out

--- src/components/AncientEgypt.jsx
+++ src/components/AncientEgypt.jsx
@@ -3,32 +3,30 @@ import './AncientEgypt.css'
 
 const AncientEgypt = () => {
     return (
-        <div class = "row">
-            <div className = "">
-                <img className="cosmetic hoverable" src="/cosmetics-egypt.png" alt="" />
+        <div class="egypt">
+
+            <div class="row">
+                <div className="col s12 m6 l12 banner">
+                    <img className="writing hoverable" src="/wall-hanging-egypt.png" alt="" />
+                </div>
             </div>
-            <div className = "">
-                <img className="cosmetic hoverable" src="/funeral-beads-egypt.png" alt="" />
-            </div>
-            <div className = "">
-                <img className="cosmetic hoverable" src="/mummified-birds-egypt.png" alt="" />
-            </div>
-            {/* <div className = "hoverable">
-                <img className="mummy" src="/mummy-egypt.png" alt="" />
-            </div> */}
-            <div>
-                <img className="mummies hoverable" src="/mummy2-egypt.png" alt="" />
-            </div>
-            <div className = "hoverable">
-                <img className="slipper" src="/slippers-egypt.png" alt="" />
-            </div>
-            <div className = "hoverable">
-                <img className="writing" src="/wall-hanging-egypt.png" alt="" />
-            </div>
-            <div className = "hoverable">
-                <img className="cosmetic" src="/writings-egypt.png" alt="" />
+            <div class="row">
+                <div class="col s12 m6 l12 midRow">
+                    <img className="mummy hoverable left" src="/mummy-egypt.png" alt="" />
+                    
+                    <img className="mummies hoverable right" src="/mummy2-egypt.png" alt="" />
+                </div>
             </div>
 
+            <div class = "row">
+                <div class = "col s12 m6 l12 botRow">
+                    <img className="slipper" src="/slippers-egypt.png" alt="" />
+                    <img className="cosmetic" src="/writings-egypt.png" alt="" />
+                    <img src="/cosmetics-egypt.png" className = "cosmetic" alt=""/>
+                    <img src="mummified-birds-egypt.png" className = "cosmetic" alt=""/>
+                    <img src="funeral-beads-egypt.png" className = "cosmetic" alt=""/>
+                </div>
+            </div>
         </div>
     );
 };
