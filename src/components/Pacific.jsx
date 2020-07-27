56975e81ac59afa3c822778b5c9275e16ab683da
jdephil <jennifer.dephillips@gmail.com>
2020-07-26 21:47:55 -0400

    pacific images on page

--- src/components/Pacific.jsx
+++ src/components/Pacific.jsx
@@ -3,14 +3,16 @@ import './Pacific.css'
 
 const Pacific = () => {
     return (
-        <div>
-            <img className='danceDress pacificArtifact' src='dance-dress-pacific.png' alt='dance dress' />
-            <img className='fan pacificArtifact' src='fan-pacific.png' alt='fan' />
-            <img className='kite pacificArtifact' src='kite-pacific.png' alt='kite' />
-            <img className='canoe pacificArtifact' src='model-canoe-pacific.png' alt='model canoe' />
-            <img className='wovenMat pacificArtifact' src='woven-mat-pacific.png' alt='woven mat' />
-            <img className='waistGarment pacificArtifact' src='waist-garment-pacific.png' alt='waist garment' />
-            <img className='pacificRoomImg' src='./pacific-room.png' alt='maori and pacific museum room' />
+        <div className='pacificDiv'>
+            <img className='fishingHook pacificArtifact' src='/fishing-hook-pacific.png' alt='fishing hook' />
+            <img className='fishingBox pacificArtifact' src='/fishing-box-pacific.png' alt='fishing box' />
+            <img className='danceDress pacificArtifact' src='/dance-dress-pacific.png' alt='dance dress' />
+            <img className='fan pacificArtifact' src='/fan-pacific.png' alt='fan' />
+            <img className='kite pacificArtifact' src='/kite-pacific.png' alt='kite' />
+            <img className='canoe pacificArtifact' src='/model-canoe-pacific.png' alt='model canoe' />
+            <img className='wovenMat pacificArtifact' src='/woven-mat-pacific.png' alt='woven mat' />
+            <img className='waistGarment pacificArtifact' src='/waist-garment-pacific.png' alt='waist garment' />
+            <img className='pacificRoomImg' src='/pacific-room.png' alt='maori and pacific museum room' />
         </div>
     );
 };
