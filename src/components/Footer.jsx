c1effdce53d883158e91edd3fbf8dd56d9263e02
Eryoung-610 <eryoung610@gmail.com>
2020-07-28 04:44:39 -0700

    team page implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { NavLink, Link } from 'react-router-dom'
 import './Footer.css'
+import './teamPage.jsx'
 
 const Footer = () => {
     return (
         <footer class="page-footer">
             <div class="container">
-                FOOTER
+                <div className="right">
+                    <Link exact to="/teampage">
+                        <div className="white-text">Meet the Team</div>
+                    </Link>
                 </div>
+            </div>
         </footer>
 
     );
