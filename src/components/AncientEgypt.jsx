49b817ff119be6bc2103a3a3973392c46cc30d8a
Caitlin Smith <caitlinelizabeth888@gmail.com>
2020-07-28 15:09:37 -0700

    added functions back to pages

--- src/components/AncientEgypt.jsx
+++ src/components/AncientEgypt.jsx
@@ -7,17 +7,36 @@ import Modal from 'react-bootstrap/Modal';
 
 const AncientEgypt = (props) => {
     const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
     const [artifacts, setArtifacts] = useState({
         name: "",
         description: "",
         imageurl: ""
     })
 
+    const handleShow = (e) => {
+        console.log(e.target.id)
+        setShow(true);
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/artifact/${e.target.id}`, artifacts)
+            .then(response => {
+                let articleTitle = response.data['dc:title'][0].value
+                let articleDescription = response.data['dc:description'][0].value
+                let articleImage = response.data['ecrm:P138_has_representation'][0].value
+                setArtifacts({ name: articleTitle, description: articleDescription, imageurl: articleImage })
+            })
+        }
+    
+        const saveToProfile = (e) => {
+            e.preventDefault()
+            axios.post(`${process.env.REACT_APP_SERVER_URL}/profile/artifact/`, artifacts)
+                
+        }
+
   
     return (
         <div class="egyptDiv">
             <div className='Modal'>
-                <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={props.handleClose}>
+                <Modal size="lg" aria-labelledby="contained-modal-title-vcenter" centered show={show} onHide={handleClose}>
                     <Modal.Header closeButton>
                     </Modal.Header>
                     <Modal.Body>
@@ -28,7 +47,7 @@ const AncientEgypt = (props) => {
                             <input type="hidden" name="name" value={artifacts.name}></input>
                             <input type="hidden" name="description" value={artifacts.description}></input>
                             <input type="hidden" name="imageurl" value={artifacts.imageurl}></input>
-                            <button type="submit" variant="outline-info" onClick={props.saveToProfile}>Save to Archive</button>
+                            <button type="submit" variant="outline-info" onClick={saveToProfile}>Save to Archive</button>
                         </form>
                     </Modal.Body>
                 </Modal>
@@ -45,30 +64,30 @@ const AncientEgypt = (props) => {
                 </div>
             </div>
 
-            <a class="modal-trigger" href="#test" onClick={props.handleShow}>
+            <a class="modal-trigger" href="#test" onClick={handleShow}>
                 <img className="egyptArtifact wallHanging hoverable" id='626417' src="egypt/wall-hanging-egypt.png" alt="wall hanging" />
             </a>
 
             <a class="modal-trigger" href="#test">
-                <img className="egyptArtifact mummies hoverable" id='30452' src="egypt/mummy2-egypt.png" alt="mummy" onClick={props.handleShow} />
+                <img className="egyptArtifact mummies hoverable" id='30452' src="egypt/mummy2-egypt.png" alt="mummy" onClick={handleShow} />
             </a>
             <a class="modal-trigger" href="#test">
-                <img className="egyptArtifact slippers hoverable" id='523725' src="egypt/slippers-egypt.png" alt="slippers" onClick={props.handleShow} />
+                <img className="egyptArtifact slippers hoverable" id='523725' src="egypt/slippers-egypt.png" alt="slippers" onClick={handleShow} />
             </a>
             <a class="modal-trigger" href="#test">
-                <img className="egyptArtifact cosmetics hoverable" id='103226' src="egypt/cosmetics-egypt.png" alt="cosmetics" onClick={props.handleShow} />
-            </a>
-
-            <a class="modal-trigger" href="#test">
-                <img className='egyptArtifact writings hoverable' id='502345' src="egypt/writings-egypt.png" alt="writings" onClick={props.handleShow}/>
+                <img className="egyptArtifact cosmetics hoverable" id='103226' src="egypt/cosmetics-egypt.png" alt="cosmetics" onClick={handleShow} />
             </a>
 
             <a class="modal-trigger" href="#test">
-                <img className="egyptArtifact mummifiedBirds hoverable" id='33177' src="egypt/mummified-birds-egypt.png" alt="mummified birds" onClick={props.handleShow} />
+                <img className='egyptArtifact writings hoverable' id='502345' src="egypt/writings-egypt.png" alt="writings" onClick={handleShow}/>
             </a>
 
             <a class="modal-trigger" href="#test">
-                <img className="egyptArtifact funeralBeads hoverable" id='523731' src="egypt/funeral-beads-egypt.png" alt="funeral beads" onClick={props.handleShow}/>
+                <img className="egyptArtifact mummifiedBirds hoverable" id='33177' src="egypt/mummified-birds-egypt.png" alt="mummified birds" onClick={handleShow} />
+            </a>
+
+            <a class="modal-trigger" href="#test">
+                <img className="egyptArtifact funeralBeads hoverable" id='523731' src="egypt/funeral-beads-egypt.png" alt="funeral beads" onClick={handleShow}/>
             </a>
 
             <a class="modal-trigger" href="#test">
