b9294efe0a5fafbad3d8fdb6ca9c15a1e2d01759
Eryoung-610 <eryoung610@gmail.com>
2020-07-28 18:00:23 -0700

    Fixes to auth, navbar

--- src/App.js
+++ src/App.js
@@ -16,7 +16,6 @@ import Signup from './components/Signup';
 import Login from './components/Login';
 import TeamPage from './components/teamPage'
 import axios from 'axios'
-
 const PrivateRoute = ({ component: Component, ...rest }) => {
   //get user via jwt token to confirm user authenticated
   const user = localStorage.getItem(`jwtToken`);
@@ -28,12 +27,10 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
       )} 
   />
 }
-
 function App() {
   // set state values
   let [currentUser, setCurrentUser] = useState("")
   let [isAuthenticated, setIsAuthenticated] = useState(true)
-
   useEffect(() => {
     let token;
     if(localStorage.getItem('jwtToken') === null) {
@@ -45,13 +42,11 @@ function App() {
       setIsAuthenticated(true);
     }
   }, [])
-
   let nowCurrentUser = (userData) => {
     console.log("oh hey this is even running")
     setCurrentUser(userData);
     setIsAuthenticated(true)
   }
-
   let handleLogout = () => {
     if(localStorage.getItem('jwtToken') !== null) {
       localStorage.removeItem('jwtToken');
@@ -59,36 +54,8 @@ function App() {
       setIsAuthenticated(false);
     }
   }
-
   console.log('Current User = ', currentUser);
   console.log('Authenticated = ', isAuthenticated);
-
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const [artifacts, setArtifacts] = useState({
-   name: "",
-   description: "", 
-   imageurl: ""
- })
- 
- const handleShow = (e) => {
-   console.log(e.target.id)
-   setShow(true);
-   axios.get(`${process.env.REACT_APP_SERVER_URL}/artifact/${e.target.id}`, artifacts)
-       .then(response => {
-           let articleTitle = response.data['dc:title'][0].value
-           let articleDescription = response.data['dc:description'][0].value
-           let articleImage = response.data['ecrm:P138_has_representation'][0].value
-           setArtifacts({ name: articleTitle, description: articleDescription, imageurl: articleImage })
-       })
-   }
-
-   const saveToProfile = (e) => {
-       e.preventDefault()
-       axios.post(`${process.env.REACT_APP_SERVER_URL}/profile/artifact/`, artifacts)
-           
-   }
-
   return (
     <div>
       <BrowserRouter>
@@ -97,13 +64,12 @@ function App() {
             <Switch>
               <Route path='/signup' component={ Signup } />
               <Route path='/login' render={ (props) => <Login {...props} nowCurrentUser={nowCurrentUser} setIsAuthenticated={setIsAuthenticated} user={currentUser} /> } />
-              <Route path='/pacific' component={Pacific} saveToProfile={saveToProfile} handleShow={handleShow} handleClose={handleClose} />
-              <Route path='/nativeAmerican' component={NativeAmerican} saveToProfile={saveToProfile} handleShow={handleShow} handleClose={handleClose}/>
-              <Route path='/ancientEgypt' exact component={AncientEgypt} saveToProfile={saveToProfile} handleShow={handleShow} handleClose={handleClose}/>
+              <Route path='/pacific' component={Pacific} />
+              <Route path='/nativeAmerican' component={NativeAmerican} />
+              <Route path='/ancientEgypt' exact component={AncientEgypt} />
               <Route path="/office" exact component={Office} />
               <Route path="/" exact component={About} />
-              <PrivateRoute path="/profile" exact component={Profile} />
-              <Route path='/teampage' component={TeamPage} />
+              <PrivateRoute path="/profile" exact component={Profile} user={currentUser} />
             </Switch>
         </div>
         <Footer />
@@ -111,5 +77,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
